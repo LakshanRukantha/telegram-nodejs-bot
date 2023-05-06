7b29534f139e6e47105949e4f49e4d976840c409
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-05-06 19:07:17 +0530

    fix: Timeout after some time

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const TelegramBot = require("node-telegram-bot-api");
-require('dotenv').config();
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
 
 const token = process.env.TOKEN;
+const port = 4000 || process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.status(200);
+  res.json({ message: "Welcome to Lakshan's telegram bot server." });
+});
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
@@ -10,4 +19,8 @@ bot.on("message", (msg) => {
   const chatId = msg.chat.id;
 
   bot.sendMessage(chatId, `${msg.text}`);
+});
+
+app.listen(port, (res) => {
+  console.log(`App is running on port ${port}`);
 });
