68392e90261857fb362ad3588a52b96fcdc43f0a
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-05-06 16:00:30 +0530

    feat: bot can read messages and reply

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("hello world!");
+const TelegramBot = require("node-telegram-bot-api");
+
+//Environment configuration
+const environment = process.env.NODE_ENV || "development";
+if (environment === "production") {
+  // Production environment configuration
+  require("dotenv").config({ path: ".env.production" });
+  console.log(`${environment} environment is running.`);
+} else {
+  // Development environment configuration
+  require("dotenv").config({ path: ".env.local" });
+  console.log(`${environment} environment is running.`);
+}
+
+const token = process.env.TOKEN;
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, `${msg.text}`);
+});
